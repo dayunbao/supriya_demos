2fafd705024270945c3dec8d4ea121c509c9dc88
Andrew Clark <dayunbao@gmail.com>
2025-02-10 20:13:11 +0100

    Added license.

--- arpeggiator_clock/arpeggiator_clock.py
+++ arpeggiator_clock/arpeggiator_clock.py
@@ -8,6 +8,21 @@ is more intuitive, as you can set BPM and quantization.
 Typical usage example:
 
   python arpeggiator_clock.py -b 120 -q 1/16 -c C#m3 -d up -r 4
+
+Copyright 2025, Andrew Clark
+
+This program is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
 

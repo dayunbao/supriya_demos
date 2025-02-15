509e3dd6835b6035aabb9c59a87af1f0fa48265f
Andrew Clark <dayunbao@gmail.com>
2025-02-15 10:00:18 +0100

    Move function

--- midi_synth_effects/midi_synth_effects.py
+++ midi_synth_effects/midi_synth_effects.py
@@ -39,21 +39,6 @@ reverb_synth: Synth
 server: Server
 synth_group: Group
 
-def scale_float(value: int, target_min: float, target_max: float):
-    """
-    Linearly scale a value from one range to another.
-
-    Args:
-        source_value (float): The value to be scaled.
-
-    Returns:
-        float: The scaled value in the target range.
-    """
-    source_min = 0
-    source_max = 127
-    
-    scaled_value = (value - source_min) * (target_max - target_min) / (source_max - source_min) + target_min
-    return round(number=scaled_value, ndigits=2)
 
 def create_effects_synths() -> None:
     """Create the effects synths.
@@ -174,6 +159,22 @@ def open_multi_inport() -> None:
     
     inports = [mido.open_input(p) for p in mido.get_input_names()]
     multi_inport = MultiPort(inports)
+
+def scale_float(value: int, target_min: float, target_max: float):
+    """
+    Linearly scale a value from one range to another.
+
+    Args:
+        source_value (float): The value to be scaled.
+
+    Returns:
+        float: The scaled value in the target range.
+    """
+    source_min = 0
+    source_max = 127
+    
+    scaled_value = (value - source_min) * (target_max - target_min) / (source_max - source_min) + target_min
+    return round(number=scaled_value, ndigits=2)
 
 if __name__ == '__main__':
     initialize_supriya()

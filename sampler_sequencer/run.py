7730a2153601f8c6d3ca2aad410109ed9a06e626
Andrew Clark <dayunbao@gmail.com>
2025-02-26 14:01:36 +0100

    Fixed arguments.

--- sampler_sequencer/run.py
+++ sampler_sequencer/run.py
@@ -1,4 +1,5 @@
 from sys import exit
+from functools import partial
 from typing import get_args
 
 from consolemenu import ConsoleMenu, MenuFormatBuilder
@@ -61,36 +62,33 @@ class TrackNumberValidator(BaseValidator):
 
         return result
 
-menu: ConsoleMenu
-sequencer: Sequencer
-server: Server
 
-def initialize_sequencer(bpm: int, quantization: str) -> None:
-    global sequencer
-    global server
-
-    sampler = Sampler(
-        midi_channels=[x for x in range(16)],
-        server=server, 
-        synth_definition=sample_player
-    )
-
-    # supriya_303 = SP303(
-    #     midi_channels=[x for x in range(16)],
-    #     server=server, 
-    #     synth_definition=sp_303
-    # )
-
-    sequencer = Sequencer(
-        bpm=bpm, 
-        instruments={sampler.name: sampler}, #  , supriya_303.name: supriya_303},
-        quantization=quantization,
-    )
-
-def create_menu() -> None:
-    global menu
+def add_track() -> None:
+    global InstrumentNameValidator
     global sequencer
 
+    prompt_util = PromptUtils(screen=menu.screen)
+    instrument_names = [name for name in sequencer.instruments.keys()]
+    track_add_validator = InstrumentNameValidator(instruments=instrument_names)
+
+    joined_names = ', '.join(instrument_names)
+    prompt = f'Enter one of the following instruments: {joined_names}'
+
+    try:
+        name, is_valid = prompt_util.input(
+            prompt=prompt,
+            enable_quit=True,
+            validators=[track_add_validator]
+        )
+        if is_valid:
+            sequencer.add_track(instrument_name=name)
+        else:    
+            prompt_util.println(f'Invalid instrument name provided: {name}')
+            return
+    except UserQuit:
+        return
+
+def create_menu(sequencer: Sequencer) -> ConsoleMenu:
     main_menu = ConsoleMenu(
         title='Sampler Sequencer', 
         subtitle='Choose an option',
@@ -143,7 +141,7 @@ def create_menu() -> None:
     ####################
     record_menu = ConsoleMenu(
         title='Record', 
-        prologue_text=create_record_menu_prologue,
+        prologue_text=partial(create_record_menu_prologue, sequencer),
         formatter=MenuFormatBuilder()
         .set_title_align('center')
         .set_subtitle_align('center')
@@ -156,18 +154,21 @@ def create_menu() -> None:
     record_change_instrument_menu_item = FunctionItem(
         text='Change instrument',
         function=change_instrument,
+        kwargs={'sequencer': sequencer},
         menu=record_menu,
     )
 
     record_change_track_menu_item = FunctionItem(
         text='Change track',
         function=change_track,
+        kwargs={'sequencer': sequencer},
         menu=record_menu,
     )
 
     record_change_both_menu_item = FunctionItem(
         text='Change instrument and track',
         function=change_both,
+        kwargs={'sequencer': sequencer},
         menu=record_menu,
     )
 
@@ -196,7 +197,7 @@ def create_menu() -> None:
     ####################
     sequencer_menu = ConsoleMenu(
         title='Sequencer Settings', 
-        prologue_text=get_current_sequencer_settings,
+        prologue_text=partial(get_current_sequencer_settings, sequencer),
         show_exit_option=False,
         clear_screen=True,
         formatter=MenuFormatBuilder()
@@ -208,10 +209,12 @@ def create_menu() -> None:
     sequencer_change_bpm_menu_item = FunctionItem(
         text='Change BPM',
         function=get_bpm_input,
+        kwargs={'sequencer', sequencer}
     )
     sequencer_change_quantization_menu_item = FunctionItem(
         text='Change quantization',
         function=get_quantization_input,
+        kwargs={'sequencer': sequencer},
     )
 
     sequencer_menu.append_item(sequencer_change_bpm_menu_item)
@@ -225,7 +228,7 @@ def create_menu() -> None:
     ####################
     tracks_menu = ConsoleMenu(
         title='Tracks',
-        prologue_text=get_current_number_of_tracks,
+        prologue_text=partial(get_current_number_of_tracks, sequencer),
         formatter=MenuFormatBuilder()
         .set_title_align('center')
         .set_subtitle_align('center')
@@ -235,8 +238,16 @@ def create_menu() -> None:
         clear_screen=True,
     )
 
-    tracks_add_menu_item = FunctionItem(text='Add track', function=add_track)
-    track_delete_menu_item = FunctionItem(text='Delete', function=delete_track)
+    tracks_add_menu_item = FunctionItem(
+        text='Add track', 
+        function=add_track,
+        kwargs={'sequencer': sequencer},
+    )
+    track_delete_menu_item = FunctionItem(
+        text='Delete', 
+        function=delete_track,
+        kwargs={'sequencer': sequencer},
+    )
 
     tracks_menu.append_item(tracks_add_menu_item)
     tracks_menu.append_item(track_delete_menu_item)
@@ -255,39 +266,21 @@ def create_menu() -> None:
     main_menu.append_item(tracks_menu_item)
     main_menu.append_item(exit_menu_item)
     
-    menu = main_menu
+    return main_menu
 
-def get_current_sequencer_settings() -> str:
-    global sequencer
-    
-    return f'Current settings:\nBeats per minute(BPM) = {sequencer.bpm} * Quantization = {sequencer.quantization}'
-
-def get_current_number_of_tracks() -> str:
-    global sequencer
-
-    
-    instruments_num_tracks_str = ''
-    for name, tracks in sequencer.tracks.items():
-        instruments_num_tracks_str += f'{name}: {len(tracks)}\n'
-
-    prologue_text = f'Current number of tracks\n{instruments_num_tracks_str}'
-
-    return prologue_text
-
-def create_record_menu_prologue() -> str:
-    global sequencer
-
+def create_record_menu_prologue(sequencer: Sequencer) -> str:
     selected_instrument_name = sequencer.get_selected_instrument_name()
     selected_track_number = f'{sequencer.get_selected_track_number() + 1}'
     record_menu_prologue_text = f'Selected instrument: {selected_instrument_name}\nSelected track number: {selected_track_number}'
 
     return record_menu_prologue_text
 
-def change_instrument() -> None:
-    global InstrumentNameValidator
-    global sequencer
+def change_both(sequencer: Sequencer) -> None:
+    change_instrument(sequencer=sequencer)
+    change_track(sequencer=sequencer)
 
-    prompt_util = PromptUtils(screen=menu.screen)
+def change_instrument(sequencer: Sequencer) -> None:
+    prompt_util = PromptUtils()
     instrument_names = [name for name in sequencer.instruments.keys()]
     instrument_name_validator = InstrumentNameValidator(instruments=instrument_names)
 
@@ -308,11 +301,8 @@ def change_instrument() -> None:
     except UserQuit:
         return
 
-def change_track() -> None:
-    global TrackNumberValidator
-    global sequencer
-
-    prompt_util = PromptUtils(screen=menu.screen)
+def change_track(sequencer: Sequencer) -> None:
+    prompt_util = PromptUtils()
     selected_instrument = sequencer.selected_instrument.name
     num_tracks = len(sequencer.tracks[selected_instrument])
     track_number_validator = TrackNumberValidator(number_of_tracks=num_tracks)
@@ -340,40 +330,8 @@ def change_track() -> None:
     except UserQuit:
         return
 
-def change_both() -> None:
-    change_instrument()
-    change_track()
-
-def add_track() -> None:
-    global InstrumentNameValidator
-    global sequencer
-
-    prompt_util = PromptUtils(screen=menu.screen)
-    instrument_names = [name for name in sequencer.instruments.keys()]
-    track_add_validator = InstrumentNameValidator(instruments=instrument_names)
-
-    joined_names = ', '.join(instrument_names)
-    prompt = f'Enter one of the following instruments: {joined_names}'
-
-    try:
-        name, is_valid = prompt_util.input(
-            prompt=prompt,
-            enable_quit=True,
-            validators=[track_add_validator]
-        )
-        if is_valid:
-            sequencer.add_track(instrument_name=name)
-        else:    
-            prompt_util.println(f'Invalid instrument name provided: {name}')
-            return
-    except UserQuit:
-        return
-
-def delete_track() -> None:
-    global TrackNumberValidator
-    global sequencer
-
-    prompt_util = PromptUtils(screen=menu.screen)
+def delete_track(sequencer: Sequencer) -> None:
+    prompt_util = PromptUtils()
 
     instrument_names = [name for name in sequencer.instruments.keys()]
     track_add_validator = InstrumentNameValidator(instruments=instrument_names)
@@ -418,12 +376,17 @@ def delete_track() -> None:
     except UserQuit:
         return
 
-def get_bpm_input() -> None:
-    global BPMValidator
-    global menu
-    global sequencer
-    
-    prompt_util = PromptUtils(screen=menu.screen)
+def exit_program(sequencer: Sequencer, server: Server) -> None:
+    """Exit the program."""
+    print('Exiting Sampler Sequencer')
+    sequencer.exit()
+    server.quit()
+    # Calling this makes sure the SuperCollider server shuts down
+    # and doesn't linger after the program exits.
+    exit(0)
+
+def get_bpm_input(sequencer: Sequencer) -> None:
+    prompt_util = PromptUtils()
     bpm_validator = BPMValidator()
     
     try:
@@ -441,11 +404,20 @@ def get_bpm_input() -> None:
     except UserQuit:
         return
 
-def get_quantization_input() -> None:
-    global QuantizationValidator
-    global menu
+def get_current_number_of_tracks(sequencer: Sequencer) -> str:
+    instruments_num_tracks_str = ''
+    for name, tracks in sequencer.tracks.items():
+        instruments_num_tracks_str += f'{name}: {len(tracks)}\n'
 
-    prompt_util = PromptUtils(screen=menu.screen)
+    prologue_text = f'Current number of tracks\n{instruments_num_tracks_str}'
+
+    return prologue_text
+
+def get_current_sequencer_settings(sequencer: Sequencer) -> str:
+    return f'Current settings:\nBeats per minute(BPM) = {sequencer.bpm} * Quantization = {sequencer.quantization}'
+
+def get_quantization_input(sequencer: Sequencer) -> None:
+    prompt_util = PromptUtils()
     quantization_validator = QuantizationValidator()
     valid_quantizations = ', '.join([q for q in get_args(Quantization)])
     
@@ -464,34 +436,37 @@ def get_quantization_input() -> None:
     except UserQuit:
         return
 
-def start_menu() -> None:
-    global menu
-    
-    menu.show()
+def initialize_instruments(server: Server) -> Sampler:
+    sampler = Sampler(
+        midi_channels=[x for x in range(16)],
+        server=server, 
+        synth_definition=sample_player
+    )
 
-def exit_program() -> None:
-    """Exit the program."""
-    global sequencer
-    global server
+    # supriya_303 = SP303(
+    #     midi_channels=[x for x in range(16)],
+    #     server=server, 
+    #     synth_definition=sp_303
+    # )
 
-    print('Exiting Sampler Sequencer')
-    sequencer.exit()
-    server.quit()
-    # Calling this makes sure the SuperCollider server shuts down
-    # and doesn't linger after the program exits.
-    exit(0)
+    return sampler
 
-def initialize_server() -> None:
-    global server
+def initialize_sequencer(server: Server, bpm: int, quantization: str) -> Sequencer:
+    sampler = initialize_instruments(server=server)
+    sequencer = Sequencer(
+        bpm=bpm, 
+        instruments={sampler.name: sampler}, #  , supriya_303.name: supriya_303},
+        quantization=quantization,
+    )
 
-    server = Server().boot()
+    return sequencer
 
 def start() -> None:
-    initialize_server()
-    initialize_sequencer(bpm=120, quantization='1/8')
-    create_menu()
-    start_menu()
-    exit_program()
+    server = Server().boot()
+    sequencer = initialize_sequencer(server=server, bpm=120, quantization='1/8')
+    main_menu = create_menu(sequencer=sequencer)
+    main_menu.show()
+    exit_program(sequencer=sequencer, server=server)
 
 if __name__ == '__main__':
     start()

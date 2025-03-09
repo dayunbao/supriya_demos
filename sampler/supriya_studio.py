a5cd9f49e2e757e13a96648290943449f94f1adb
Andrew Clark <dayunbao@gmail.com>
2025-03-09 13:26:25 +0100

    Rename directory.

--- sampler/supriya_studio.py
+++ sampler/supriya_studio.py
@@ -15,7 +15,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License 
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
-from concurrent.futures import Future
 from pathlib import Path
 
 from midi_handler import MIDIHandler

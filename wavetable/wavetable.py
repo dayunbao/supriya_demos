e7bb536486051d6130b59a621fa62669b9e956aa
Andrew Clark <dayunbao@gmail.com>
2025-06-21 10:42:33 +0200

    Finishing touches.

--- wavetable/wavetable.py
+++ wavetable/wavetable.py
@@ -18,7 +18,6 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 import random
 import sys
 from math import pi
-from pprint import pprint
 
 from supriya import (
     AddAction, 
